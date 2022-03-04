4c510035b548b1a1c2131271d4fab883f08e8bcb
Teresa0423 <teresawang0423@gamil.com>
2022-03-04 15:57:30 +0800

    calculate student score NG

---
+++ src/main/java/fju/com/NG.java
@@ -0,0 +1,13 @@
+package fju.com;
+
+public class NG {
+    public static void main(String[] args){
+        String names[] = {"Hank, Tom, Mary, Teresa, Mark"};
+        int English[] = {40,60,70,80,50};
+        int Math[] = {70,60,60,20,40};
+        for (int i = 0; i < 5; i++) {
+            System.out.println
+                    (names[i] + "/t" + English[i] + "/t" + Math[i] + "/t" + (English[i]+Math[i])/2);
+        }
+    }
+}
