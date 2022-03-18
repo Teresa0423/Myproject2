e3bb8938f3582f76f49863ad7d57a1189b8a3881
Teresa0423 <teresawang0423@gmail.com>
2022-03-18 15:48:48 +0800

    SileCustomer design

--- src/main/java/fju/com/cus/SileCustomer.java
+++ src/main/java/fju/com/cus/SileCustomer.java
@@ -1,7 +1,16 @@
 package fju.com.cus;
 
-public class SileCustomer {
-    public static void main(String[] args){
-        Customer c1 = new Customer("0001",2000);
+public class SileCustomer extends Customer{
+    //public static void main(String[] args){
+    //    Customer c1 = new Customer("0001",2000);
+    //    Customer c2 = new Customer("0002",4500);
+    //}
+
+    public SileCustomer (String id,int amount){
+        super(id, amount);
+    }
+
+    public int getTotal(){
+        return amount;
     }
 }
