70f610f569ecf136f1e2447160802ebe1f4eee65
Teresa0423 <teresawang0423@gmail.com>
2022-04-01 15:25:45 +0800

    swich or if else to design Tester

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args){
-        int duff = Math.abs(100-300); //絕對值Math.abs
+        int duff = Math.abs(100-300);
         System.out.println(duff);
     }
 }
--- src/main/java/fju/com/ticket/Tester.java
+++ src/main/java/fju/com/ticket/Tester.java
@@ -1,5 +1,6 @@
 package fju.com.ticket;
 
+import java.lang.invoke.SwitchPoint;
 import java.util.Scanner;
 
 public class Tester {
@@ -9,5 +10,43 @@ public class Tester {
                 Station.KAOHSIUNG_STATION);
         Scanner scanner = new Scanner(System.in);
         System.out.println("Your start station?"+"(1.Taipei 2.Taichung 3.Kaohsiung)");
+        int choice = Integer.parseInt(scanner.next());
+        Station start = null;
+        switch(choice){
+            case 1:
+                start = Station.TAIPEI_STATION;
+                break;
+            case 2:
+                start = Station.TAICHUNG_STATION;
+                break;
+            case 3:
+                start = Station.KAOHSIUNG_STATION;
+                break;
+        }
+        System.out.println("Your destination station?"+"1.Taipei 2.Taichung 3.Kaohsiung");
+        choice = Integer.parseInt(scanner.next());
+        Station destination = null;
+        switch (choice){
+            case 1:
+                destination = Station.TAIPEI_STATION;
+                break;
+            case 2:
+                destination = Station.TAICHUNG_STATION;
+                break;
+            case 3:
+                destination = Station.KAOHSIUNG_STATION;;
+                break;
+        }
+        System.out.println("Which kind of ticket?"+"1.Normal ticket 2.Student ticket 3.Elder ticket");
+        choice = Integer.parseInt(scanner.next());
+        ticket = null;
+        switch (choice){
+            case 1:
+                ticket = new Ticket(start,destination);
+                break;
+            case 2:
+                ticket = new Ticket(start,destination);
+                break;
+        }
     }
 }
