0e401f0efb82b258639449c57b9f5b4a4cc87916
Teresa Wnag <teresawang0423@gmail.com>
2022-05-13 16:22:24 +0800

    parking amount

--- src/main/java/fju/com/parking/Runner.java
+++ src/main/java/fju/com/parking/Runner.java
@@ -7,8 +7,8 @@ import java.util.Date;
 public class Runner {
     public static void main(String[] args) throws ParseException {
         String id = "ABC-1234";
-        String entertime = "07:58";
-        String exittime = "11:00";
+        String entertime = "08:00";
+        String exittime = "11:15";
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
         //Date d = new Date();
         Date d = null;
@@ -26,19 +26,18 @@ public class Runner {
             System.out.println(d2.getTime());
             int mins = (int)(d2.getTime()-d.getTime())/1000/60;
             System.out.println(mins);
-            if (mins >= 15){
-                int fee = 30*(mins/60)+30;
+            //3:14 => 90, 3:15 => 120
+            //3:15-3:30 =>105
+            if (mins >= 195){
+                int fee = 30*(mins/60)+15;
                 System.out.println("Fee :"+fee);
             }else{
                 int fee = 30*(mins/60);
                 System.out.println("Fee :"+fee);
             }
-
-
             //int fee = 30*(mins/60);
             //System.out.println("Fee: "+fee);
-            //3:14 => 90, 3:15 => 120
-            //3:15-3:30 =>105
+
         }catch(Exception e){
             System.out.println("exit");
         }
