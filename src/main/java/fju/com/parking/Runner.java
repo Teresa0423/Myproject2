8843e3d6239fee9dcde2414316e0755e6df6bac9
Teresa Wnag <teresawang0423@gmail.com>
2022-05-13 14:25:39 +0800

    parking,date and gettime

---
+++ src/main/java/fju/com/parking/Runner.java
@@ -0,0 +1,18 @@
+package fju.com.parking;
+
+import java.util.Date;
+
+public class Runner {
+    public static void main(String[] args){
+        String id = "ABC-1234";
+        String entertime = "08:00";
+        String exittime = "11:00";
+        Date d = new Date();
+        System.out.print(d);
+        Date d2 = new Date();
+        System.out.println(d2);
+        System.out.println(d2.getTime());
+
+
+    }
+}
