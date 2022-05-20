c6a7332a8696bb655ae3dfa13932249a56f6dbad
Teresa Wnag <teresawang0423@gmail.com>
2022-05-20 14:17:11 +0800

    interface learning 1

---
+++ src/main/java/fju/com/gui/Myinterface.java
@@ -0,0 +1,7 @@
+package fju.com.gui;
+
+public interface Myinterface {
+    public void test();
+
+    public int max (int a, int b);
+}
---
+++ src/main/java/fju/com/gui/Mytest.java
@@ -0,0 +1,12 @@
+package fju.com.gui;
+
+public class Mytest implements Myinterface{
+    public void test(){
+
+    }
+
+    @Override
+    public int max(int a, int b) {
+        return 0;
+    }
+}
