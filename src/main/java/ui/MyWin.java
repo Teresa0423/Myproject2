10deba2a947efb65830dd5caa8e7744f23f1157f
Teresa Wnag <teresawang0423@gmail.com>
2022-05-20 15:10:08 +0800

    ui try frame1

--- src/main/java/ui/MyWin.java
+++ src/main/java/ui/MyWin.java
@@ -5,10 +5,16 @@ import javax.swing.*;
 public class MyWin {
     public static void main(String[] args){
         JFrame frame = new JFrame();
+        //設定高度、寬度
+        frame.setSize(600,400);
+        frame.setLocation(300,200);
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        // add component
+        JButton button = new JButton("OK!");
+        //add button to frame
+        frame.add(button);
         //顯示JFrame
         frame.setVisible(true);
-        //設定高度、寬度
-        frame.setSize(200,100);
         System.out.println("End?");
     }
 }
