2f9cea919d9f0feabaa8bb4349e3ee14c3617405
Teresa Wnag <teresawang0423@gmail.com>
2022-05-20 14:48:44 +0800

    ui try frame

--- src/main/java/ui/MyWin.java
+++ src/main/java/ui/MyWin.java
@@ -5,7 +5,10 @@ import javax.swing.*;
 public class MyWin {
     public static void main(String[] args){
         JFrame frame = new JFrame();
+        //顯示JFrame
         frame.setVisible(true);
+        //設定高度、寬度
+        frame.setSize(200,100);
         System.out.println("End?");
     }
 }
